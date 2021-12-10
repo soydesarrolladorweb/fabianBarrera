844dc9aa788750e9ebe66a880583d35342041368
fabianbarrera <febarrera12@misena.edu.co>
2021-12-09 23:49:56 -0500

    API formsubmit backend js contactForm

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,3 +173,32 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+/*==================== Contact Form  ====================*/
+const $form = document.querySelector(".contact__form"),
+        $loader = document.querySelector(".contact__form-loader"),
+        $modalContent = document.querySelector(".contacts__modal-content");
+
+     $form.addEventListener("submit",(e) => {
+        e.preventDefault();
+        $loader.classList.remove(".none")
+        fetch("https://formsubmit.co/ajax/proyectosweb.fabian@gmail.com",{
+            method:"POST",
+            body:new FormData(e.target)
+        }).then((res) => (res.ok ? res.json(): Promise.reject(res)))
+        .then((json) =>{
+            console.log(json);
+            location.hash = "#gracias";
+            $form.reset();
+        })
+        .catch((err) =>{
+            console.log(err);
+            let message = err.statusText||"Ocurrió un error al enviar, intenta nuevamente"
+            $modalContent.querySelector("h4").innerHTML = `Error ${err.status}: ${message}`;
+        }).finally(() =>{
+            $loader.classList.add("none");
+            setTimeout(()=>{
+                location.hash = "#close";
+        },4000);
+
+    });
+});
